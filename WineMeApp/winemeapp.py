54aeaac764bb011bd051bb297aa4f5cca38ede93
Ivan PG <pintogriloivan@gmail.com>
2024-04-24 21:43:39 +0200

    Update winemeapp.py

--- WineMeApp/winemeapp.py
+++ WineMeApp/winemeapp.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 from funciones_sistema_recomendacion_vinos import *
 import requests
 
-data = pd.read_csv("WineMeApp/df_vinos_modelos.csv")
+data = "df_vinos_modelos.csv"
 
 
 # Tab Info
