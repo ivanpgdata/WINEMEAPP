4ab263599e71ba87e70e7e5b7d44fee48e2311fa
Ivan PG <pintogriloivan@gmail.com>
2025-02-24 22:20:18 +0100

    Update winemeapp.py

--- WineMeApp/winemeapp.py
+++ WineMeApp/winemeapp.py
@@ -172,29 +172,33 @@ if selected == "Quiénes somos":
     col1, col2, col3 = st.columns([0.3, 0.3, 0.3])
     with col1:  # To display the header text using css style
         with st.container():
-            # Importar imagen desde GDrive
-            file_maria_id = "1Laowp1RwnEVZ9iqUkJhs7oBlNQ7gmPN0"
-            url_pic_maria = f"https://drive.google.com/file/d/{file_maria_id}/view"
-            response_maria = requests.get(url_pic_maria)
-            st.image(response_maria.content)
-	    # st.image(WineMeApp/images/maria_img.jpg)
-            st.markdown(
-                """ #### María Pérez Sebastián :computer:""")
-            st.write("""
-            Actualmente estoy dando un giro a mi carrera profesional formándome como Data Scientist. 
-            """)
-            st.write("""
-            Anteriormente, me he dedicado casi 10 años al desarrollo integral de proyectos arquitectónicos. Me considero una persona 
-            responsable, comprometida y con una alta capacidad de trabajo. Me gusta trabajar en equipo, aportar ideas personales y aprender de la 
-            forma de trabajar de los otros miembros positivamente.
-            """)
-            st.write("""[Github](https://github.com/marpezseb),
-                 [LinkedIn](https://www.linkedin.com/in/-mps2024/) """)
+		# Importar imagen desde GDrive
+		# file_maria_id = "1Laowp1RwnEVZ9iqUkJhs7oBlNQ7gmPN0"
+		# url_pic_maria = f"https://drive.google.com/file/d/{file_maria_id}"
+		file_maria_id = "AKGpihaIDs3qobDlR7Y8Oeva2nPwi2Ul1AM7nPT5jj9mRm16R2R-yNByT8-gLM-p3HddEPLic1h9WSeyWglDrfJiGliJU4F_xpoXhck=w1920-h919"
+		url_pic_maria = f"https://drive.google.com/drive-viewer/{file_maria_id}"
+		response_maria = requests.get(url_pic_maria)
+		st.image(response_maria.content)
+		# st.image(WineMeApp/images/maria_img.jpg)
+		st.markdown(
+		""" #### María Pérez Sebastián :computer:""")
+		st.write("""
+		Actualmente estoy dando un giro a mi carrera profesional formándome como Data Scientist. 
+		""")
+		st.write("""
+		Anteriormente, me he dedicado casi 10 años al desarrollo integral de proyectos arquitectónicos. Me considero una persona 
+		responsable, comprometida y con una alta capacidad de trabajo. Me gusta trabajar en equipo, aportar ideas personales y aprender de la 
+		forma de trabajar de los otros miembros positivamente.
+		""")
+		st.write("""[Github](https://github.com/marpezseb),
+		 [LinkedIn](https://www.linkedin.com/in/-mps2024/) """)
     with col2:
         # Importar imagen desde GDrive
 
-        file_ivan_id = "1Ti7uUVWqLwREAhcr_cUZ8zjxasXPhc3q"
-        url_pic_ivan = f"https://drive.google.com/file/d/{file_ivan_id}/view"
+        # file_ivan_id = "1Ti7uUVWqLwREAhcr_cUZ8zjxasXPhc3q"
+        # url_pic_ivan = f"https://drive.google.com/file/d/{file_ivan_id}"
+	file_ivan_id = "AKGpihZ2cOe8zowvZLgLlxRTJO0ouTUy4NFMRlDZ6NQjt6DkU_aGoJuBO8iYWFz-ZruPeN3FbECWGqav3AjEqrXMcyJYdx0vl-dgvYU=w1920-h919"
+        url_pic_ivan = f"https://drive.google.com/drive-viewer/{file_ivan_id}"
         response_ivan = requests.get(url_pic_ivan)
         st.image(response_ivan.content)
 	# st.image(WineMeApp/images/ivan_img.jpg)
@@ -212,9 +216,10 @@ if selected == "Quiénes somos":
     with col3:
         # Importar imagen desde GDrive
 	    
-        file_soraya_id = "1Apq3V5kKbWVdoH-k1d3TV6I-rFggKUAb"
-        url_pic_soraya = f"https://drive.google.com/file/d/{file_soraya_id}/view"
-	
+        # file_soraya_id = "1Apq3V5kKbWVdoH-k1d3TV6I-rFggKUAb"
+        # url_pic_soraya = f"https://drive.google.com/file/d/{file_soraya_id}"
+	file_soraya_id = "AKGpihY9O7SxLgpaS0mRkjVF9lPlzAdq9_eJMV-0qwagsljiv4NR92cZrXUleXP3fhWnQFFz1nOhnm0onWHbWKnp11eNTj6PgkEmy4E=w1920-h919"
+        url_pic_soraya = f"https://drive.google.com/drive-viewer/{file_soraya_id}"
         response_soraya = requests.get(url_pic_soraya)
         st.image(response_soraya.content)
 	# st.image(WineMeApp/images/soraya_reshape1.jpg)
